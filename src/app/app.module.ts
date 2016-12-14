a3b1ade21ca4bbf6b3a3d05f1a93d06713958792
David Sudia <dsudia@gmail.com>
2016-12-13 22:14:46 -0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,20 @@ import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TechnologiesComponent } from './technologies/technologies.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    HomeComponent,
+    NavbarComponent,
+    TechnologiesComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
